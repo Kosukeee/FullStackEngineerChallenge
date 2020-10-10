6ac3538075917d55e31b0edc0a37ec9ad7a63292
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2020-10-10 21:22:49 +0900

    Fixed - Separate employee controllere file

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 const keys = require("../config/keys");
 
 const validateLoginInput = require("../validation/login");
-const validateEmployeeInput = require("../validation/employee");
 const User = require("../models/user");
 const Employee = require("../models/employee");
 
@@ -52,82 +51,4 @@ exports.postLogin = async (req, res, next) => {
       }
     });
   });
-};
-
-const createaError = () => {
-  const error = new Error();
-  error.statusCode = 500;
-
-  return error;
-};
-
-exports.postEmployee = (req, res, next) => {
-  const { errors, isValid } = validateEmployeeInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const { name } = req.body;
-  const { email } = req.body;
-  const { password } = req.body;
-  const { evaluation } = req.body;
-  const { isReviewer } = req.body;
-
-  const employee = new Employee({
-    name,
-    email,
-    password,
-    evaluation,
-    isReviewer,
-  });
-
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(employee.password, salt, (err, hashedPw) => {
-      if (err) throw err;
-      employee.password = hashedPw;
-      employee
-        .save()
-        .then((user) => res.json(user))
-        .catch((err) => console.log(err));
-    });
-  });
-};
-
-exports.deleteEmployee = (req, res, next) => {
-  const employeeId = req.params.employeeId;
-
-  Employee.findByIdAndDelete(employeeId)
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      const error = createaError(err);
-      return next(error);
-    });
-};
-
-exports.putEmployee = (req, res, next) => {
-  const employeeId = req.params.employeeId;
-
-  Employee.findByIdAndUpdate(employeeId, req.body, { new: true })
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      const error = createaError();
-      return next(error);
-    });
-};
-
-exports.getEmployees = (req, res, next) => {
-  Employee.find()
-    .then((employees) => {
-      return res.json({ employees });
-    })
-    .catch((err) => {
-      const error = new Error(err);
-      error.statusCode = 500;
-      next(error);
-    });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const authController = require("../controllers/auth");
 const feedbackController = require("../controllers/feedback");
+const employeeController = require("../controllers/employee");
 
 const router = express.Router();
 
 router.post("/login", authController.postLogin);
 
 // add, remove, update, get Empoloyee
-router.post("/api/admin/employee", authController.postEmployee);
+router.post("/api/admin/employee", employeeController.postEmployee);
 
-router.get("/api/employees", authController.getEmployees);
+router.get("/api/employees", employeeController.getEmployees);
 
-router.delete("/api/admin/employee/:employeeId", authController.deleteEmployee);
+router.delete(
+  "/api/admin/employee/:employeeId",
+  employeeController.deleteEmployee
+);
 
-router.put("/api/admin/employee/:employeeId", authController.putEmployee);
+router.put("/api/admin/employee/:employeeId", employeeController.putEmployee);
 
 // add feedbacks
 router.post("/api/feedback", feedbackController.postFeedback);
